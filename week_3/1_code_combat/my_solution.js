5cc76794cd7eb224d8c70bd7254276b0256c674d
mlear <m.lear09@gmail.com>
2014-04-14 18:58:51 -0500

    Update my_solution.js

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -1,11 +1,53 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge by myself
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
  
+// rescue mission
+// delete move right
+// move up
+// move right
+// move right
+// move down
+// attack
+
+this.moveUp();
+this.moveRight();
+this.moveRight();
+this.moveDown();
+this.attackNearbyEnemy();
+
+
+// grab the mushroom
+// move right
+// move left
+// move up
+// attack
+
+this.moveRight();
+this.moveLeft();
+this.moveUp();
+
+this.attackNearbyEnemy();
+
+// Drink Me
+// attack
+// move right
+// move down
+// move up
+// move right
+// attack
  
+this.attackNearbyEnemy();
+this.moveRight();
+this.moveDown();
+this.moveUp();
+this.moveRight();
+this.attackNearbyEnemy();
+
+
  
  
  
