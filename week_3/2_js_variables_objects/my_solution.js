5c622ca03361d9072d3ecb77ed88470dfc401f63
Martin Lear <m.lear09@gmail.com>
2014-04-15 23:13:16 -0500

    Paired Peer with Zach

--- week_3/2_js_variables_objects/my_solution.js
+++ week_3/2_js_variables_objects/my_solution.js
@@ -1,20 +1,28 @@
-// I paired [by myself, with:] on this challenge.
+// I paired with Zach Pflederer on this challenge.
 
 
 
 
 // Pseudocode
-// 
-// 
-// 
-// 
+// We will declare a variable called secretNumber equal to 1
+// We will set secretNumber equal to 7
+// We will create a new variable called password and we will set it equal to "password"
+// We will set password equal to "just open the door"
+// We will create a new variable called allowedIn and we will set it to true.
+// We will set allowedIn to false.
+// We will create a new variable memmbers and set it equal to 2.
+// We will set members equal to an array of 1, 2, 3.
+// We will set the first value of members to "John".
+// We will set the forth value of members to "Mary".
 
 
 // __________________________________________
 // Write your code below.
 
-
-
+var secretNumber = 7;
+var password = "just open the door";
+var allowedIn = false;
+var members = ["John", 2, 3, "Mary"];
 
 
 
@@ -22,19 +30,19 @@
 // __________________________________________
 // Refactored Code: Include a refactored version (or justification of your original code) here. 
 
-
+// The code is already as simple as it possibly can be.
 
 
 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
 // 
-// 
-// 
-// 
-// 
-// 
-
+//  This exercise was a great introduction to Javascript and Test Driven Development.
+//  I paired with Zach on this, and we had fun trying to predict the variables that were
+//  going to asked for.  He was a great help and resource, and can't wait to pair with
+//  him again.  We did run into one snag when I was driving however, we kept getting a 
+//  runtime error.  After resetting my command line, I quickly realized that it was me,
+//  not typing node before my_solution.js, and it was quickly fixed.
 
 // __________________________________________
 // Driver Code:  Do not alter code below this line.
