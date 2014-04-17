ee0bab7e1493acaa890060e053b82d2b14dbfb68
mlear <m.lear09@gmail.com>
2014-04-17 16:48:18 -0500

    Update my_solution.js

--- week_3/4_create_code_combat_mission/my_solution.js
+++ week_3/4_create_code_combat_mission/my_solution.js
@@ -1,16 +1,19 @@
-// I worked [by myself, with:] on this challenge
+// I worked by myself on this challenge
 
 // Your mission description:
-// 
-// 
+//  This mission is to sneak up on the munchkin without him noticing, then attack the munchkin and do a victory dance.  
+//  The munchkin will be patrolling the hallway, don't get caught!
 // 
 
 // Pseudocode
 // 
-// 
-// 
-// 
-// 
+// We will define variables to create a hero and a munchkin. 
+// The variables will include values that indicate the position of the vaiable to be displayed on the screen.
+// The user will need to input direction to the hero variable to allow the hero to change position on the sreen.
+// We will define functions to support the movement and actiona of the variables.
+// We will need seperate functions for movement on both the x and y access, along with the actions to attack and dance.
+// We will need to compare the variables positions and actions to determine if the user was successful.
+// We will need to display to the user the outcome of the mission.
 
 // Initial Code
 
